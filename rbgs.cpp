219569bdbb6cbc531708d439cd70eb1a66514026
nivesh <nidhinivesh.d@gmail.com>
2016-11-21 13:36:39 +0100

    Add files via upload

---
+++ rbgs.cpp
@@ -0,0 +1,32 @@
+int main(int argc, char *argv[]){
+  
+//grid object
+  //vector of all node values
+  //constructor (size_x, size y)
+    //vector (size_x*size_y)
+  //operator(i,j) to access values
+  //data members: size_x, size_y -> h_x = 2/(size_x-1), h_y = 1/(size_y-1);
+  //
+  
+//define pi
+//define f = 4*pi*pi*sin(2*pi*h_x*i)*sinh(2*pi*j*h_y)
+  
+//
+  
+//setboundaryconditions function
+   //set non zero dirichlet
+   //loop over y=1 boundary
+      //set to sin(2*pi*h_x*i)*sinh(2*pi*j*h_y)
+  
+//RBGS
+  //loop over red
+    //calculate u based on neighbours
+    //need stencil values
+  
+  //loop over black
+    
+  
+//calculate error norm
+  //loop over inner points
+
+}
