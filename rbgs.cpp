131b79012230d8a727b6f6e43a4fb2a488ede43d
Sachin Sanil <yx11ufeg@stud.informatik.uni-erlangen.de>
2016-11-30 17:33:24 +0100

    Split Start

--- rbgs.cpp
+++ rbgs.cpp
@@ -26,22 +26,6 @@ int main(int argc, char *argv[]){
   
   
   
-//grid object
-  //vector of all node values
-
-  //operator(i,j) to access values. write a get function as well. eg. A.get(i,j) returning data vector
-    //A.get(i,j) = data(j*n_x + i)
-  //data members: n_x, n_y
-  //data members: h_x = 2/n_x, h_y = 1/n_y;
-    //n_x and n_y and no. of intervals. no. of grid points then is n_x+1 and n_y+1
-  
-  //constructor (size_x, size y)
-    //data vector (n_x+1)*(n_y+1)
-  
-  //need get_f function which evaluates right handside
-    //A.get_f(i,j) = 4*pi*pi*sin(2*pi*h_x*i)*sinh(2*pi*j*h_y)
-
-  
   
 //setboundaryconditions function
   set_boundary(u); 
